3e813dd4d4c7c4130f658be837a1c14200f031ea
Ayas Oukache <ayasouk.iam@gmail.com>
2024-01-14 14:24:01 +0100

    Readapt Register Callback script for mainnet

--- cloud-worker/shyft/register-callback.js
+++ cloud-worker/shyft/register-callback.js
@@ -4,15 +4,21 @@ myHeaders.append("x-api-key", process.env.SHYFT_API_KEY);
 myHeaders.append("Content-Type", "application/json");
 
 var raw = JSON.stringify({
-  "network": "devnet",
+  "network": "mainnet-beta",
   "addresses": [
-    ADRESSES_TO_TRACK_1
+    process.env.TRACK_WALLET_1,
+    process.env.TRACK_WALLET_2,
+    process.env.TRACK_WALLET_3
   ],
-  "callback_url": "YOUR CALLBACK URL",
+  "callback_url": "https://test-shyft-1.kachou-help.workers.dev",
   "events": [
     "SWAP",
     "SOL_TRANSFER"
-  ]
+  ],
+  "enable_raw":true,
+  "enable_events":true,
+  "type":"CALLBACK",
+  "encoding":"RAW"
 });
 
 var requestOptions = {
